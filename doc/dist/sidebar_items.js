7c6d23a823142a1b7ce441a23ce0a8f762cd10a4
Kash Nouroozi <knouroozi@gmail.com>
2015-08-24 17:43:28 -0400

    Docs! :shipit:

---
+++ doc/dist/sidebar_items.js
@@ -0,0 +1,9 @@
+sidebarNodes = {
+  "modules": [
+    {"id": "Proplist",
+"docs": ["delete/2","delete/3","delete_first/2","drop/2","equal?/2","fetch/2","fetch!/2","get/3","get_values/2","has_prop?/2","merge/2","merge/3","new/0","new/1","new/2","pop/3","pop_first/3","proplist?/1","props/1","put/3","put_new/3","split/2","take/2","update/4","update!/3","values/1"]
+
+  }]
+
+};
+fillSidebarWithNodes(sidebarNodes);
